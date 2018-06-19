434bcd52e798cd1b0e6449a16b7fea73ef94bfad
JeffMinsungKim <jeffminsungkim@gmail.com>
2018-06-19 11:33:52 +0900

    add navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="success">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">
+      <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="BV">
+      VoiceCaddie
+    </b-navbar-brand>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav>
+        <b-nav-item href="#">Link 1</b-nav-item>
+        <b-nav-item href="#">Link 2</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using button-content slot -->
+          <template slot="button-content">
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Signout</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<style scoped>
+.navbar {
+  padding: 8px;
+  height: 50px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.26);
+}
+
+.navbar-brand {
+  font-family: Roboto,"Helvetica Neue", sans-serif;
+  padding-left: 20px;
+}
+
+</style>
