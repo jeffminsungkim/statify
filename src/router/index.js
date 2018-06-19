5cb7c8be12d6d7f2be866859584486c87b1ffce9
JeffMinsungKim <jeffminsungkim@gmail.com>
2018-06-19 11:32:07 +0900

    status log routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import DashBoard from '@/pages/dashboard/Index'
+import DashBoard from '@/views/dashboard/Index'
+import AppLog from '@/views/status-log/app/Index'
+import DeviceLog from '@/views/status-log/device/Index'
 
 Vue.use(Router)
 
@@ -8,9 +10,21 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/dashboard',
+      path: '/',
       name: 'DashBoard',
       component: DashBoard
+    },
+
+    {
+      path: '/applog',
+      name: 'AppLog',
+      component: AppLog
+    },
+
+    {
+      path: '/devicelog',
+      name: 'DeviceLog',
+      component: DeviceLog
     }
   ]
 })
