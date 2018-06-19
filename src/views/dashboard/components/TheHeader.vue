77fbf1e166a02fb25435bbba6bbea5d3bc0d03c6
JeffMinsungKim <jeffminsungkim@gmail.com>
2018-06-19 11:32:59 +0900

    add dashboard header

---
+++ src/views/dashboard/components/TheHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="page-header">
+    <div id="page-header-inner">
+      <h2 class="page-header">
+        Voice Caddie Status Dashboard
+      </h2>
+      <p>
+        This page provides status information on the services that are part of Voice Caddie.
+        Check back here to view the current status of the services listed below.
+        For additional information on these services, please visit <a href="#">voicecaddie.com</a>.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: block;
+  }
+
+  #page-header {
+    font-family: Roboto,"Helvetica Neue", sans-serif;
+    background-color: #fff;
+    box-shadow: 0 1px 0 0 rgba(0,0,0,.1);
+    padding: 64px 0;
+  }
+
+  #page-header p {
+    color: rgba(0,0,0,0.54);
+    font-size: 16px;
+    line-height: 24px;
+    max-width: 600px;
+  }
+
+   #page-header a {
+    text-decoration: none;
+    color: rgba(0,0,0,0.87);
+  }
+
+  #page-header-inner {
+    margin: 0 auto;
+    padding: 0 12px;
+    max-width: 978px;
+  }
+
+  #page-header h2 {
+    font-size: 28px;
+    font-weight: 500;
+    line-height: 36px;
+    margin: 0 0 16px 0;
+  }
+
+</style>
