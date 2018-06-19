4f0cce273aa39c895eead913ae934be7015bd1a6
JeffMinsungKim <jeffminsungkim@gmail.com>
2018-06-19 11:31:28 +0900

    add pages for status log

---
+++ src/views/status-log/app/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <the-side-navbar></the-side-navbar>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import TheSideNavbar from '@/components/TheSideNavbar'
+export default {
+  components: {
+    TheSideNavbar
+  },
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Hello, App Log Page :)'
+    }
+  }
+}
+</script>
---
+++ src/views/status-log/device/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Hello, Device Log Page :)'
+    }
+  }
+}
+</script>
